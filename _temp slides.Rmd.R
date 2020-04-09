dc506102cf3e4e93909ddccf4e7abbcd562696b2
Luna Sare <sanchez.reyes.luna@gmail.com>
2020-04-08 20:06:28 -0700

    blogdown tutorial slides

---
+++ _temp slides.Rmd.R
@@ -0,0 +1,23 @@
+class: split-20
+
+.row[.content.vmiddle.center[
+# Adding publications, projects, a blog section, the [sky is the limit]...
+]]
+
+.row.split-90[
+.row.split-60.bg-main2[
+.column[.content.nopadding.center.vmiddle[
+<img src="media/blogdown1.gif" width="100%" />
+]]
+.column.split-two[
+.row[.content.vmiddle[
+### `r emo::ji("point_left")` Content that is linked on the widgets is stored in folders on the `content/` folder
+]]
+.row[.content[
+### There are predetermined folders (courses, blog posts, projects, publications, slides and talks), but you can add as many as you need.
+]]
+]
+.row[.content[
+]]
+]
+]
